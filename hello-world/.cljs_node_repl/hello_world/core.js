0f9f672b6da52d3eaeb3d5fc3e12522973bdb2b9
Rajeev Atla <rajeev@rajeevatla.com>
2022-06-07 06:16:34 +0000

    initial commit

---
+++ hello-world/.cljs_node_repl/hello_world/core.js
@@ -0,0 +1,8 @@
+// Compiled by ClojureScript 1.10.758 {:target :nodejs}
+goog.provide('hello_world.core');
+goog.require('cljs.core');
+cljs.core.load_file(".cljs_node_repl/cljsjs/react-dom/development/react-dom.inc.js");
+hello_world.core.global$module$react_dom = goog.global["ReactDOM"];
+ReactDOM.render(React.createElement("h2",null,"Hello, React!"),document.getElementById("app"));
+
+//# sourceMappingURL=core.js.map
